51104b7500dc77fafe0b0eff7001ff9068b90d85
BATTAlalitha <143865731+BATTAlalitha@users.noreply.github.com>
2025-02-16 22:12:17 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,8 @@
+T = int(input())  # Number of test cases
+
+for _ in range(T):
+    X = int(input())  # Time when Janmansh starts
+    if X + 3 <= 10:
+        print("Yes")
+    else:
+        print("No")
